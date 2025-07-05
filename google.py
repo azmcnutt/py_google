c1e3154df2a09590e8bd3dacf15a4cd65289238a
James M <31522486+azmcnutt@users.noreply.github.com>
2025-07-05 13:31:07 -0700

    Create google.py

---
+++ google.py
@@ -0,0 +1,15 @@
+# -*-coding: utf-8 -*-
+
+"""
+Basic Python wrapper for interacting with Google API
+"""
+
+__author__ = "James McNutt"
+__copyright__ = "\u00A9 2025 James McNutt"
+__credits__ = ["James McNutt",]
+__license__ = "MIT License"
+__version__ = "0.0.1"
+__maintainer__ = "James McNutt"
+__email__ = "azmcnutt@gmail.com"
+__status__ = "Prototype"
+
